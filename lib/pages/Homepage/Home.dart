ccf2fa1af669181d2b4df4db268d89f8adc78e38
Aman-786 <kumaraman33063@gmail.com>
2023-04-04 14:39:47 +0530

    ytplayer setup but little updates remain

--- lib/pages/Homepage/Home.dart
+++ lib/pages/Homepage/Home.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as services;
+import 'package:flutter/services.dart';
 import 'package:gjustit3/common/color.dart';
 import 'package:gjustit3/model/quotesmodel.dart';
 import '../../common/textHeadingMain.dart';
--- lib/pages/youtube/youtube.dart
+++ lib/pages/youtube/youtube.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gjustit3/common/color.dart';
+import 'package:gjustit3/pages/Homepage/Home.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:flutter/services.dart';
 
@@ -15,6 +16,8 @@ class _YtPlayerState extends State<YtPlayer> {
 
   @override
   void initState() {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeLeft]);
     final videoID = YoutubePlayer.convertUrlToId(widget.url);
     _controller = YoutubePlayerController(
       initialVideoId: videoID!,
@@ -29,21 +32,40 @@ class _YtPlayerState extends State<YtPlayer> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  void dispose() {
     SystemChrome.setPreferredOrientations(
-        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeLeft]);
-    return Scaffold(
-      backgroundColor: AppColor.backgroundColor,
-      body: SafeArea(
-        child: YoutubePlayerBuilder(
-            player: YoutubePlayer(controller: _controller),
-            builder: (context, player) {
-              return Scaffold(
-                body: Container(
-                  child: player,
-                ),
-              );
-            }),
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    super.dispose();
+  }
+
+  Future<bool> _onwill() async {
+    return (await Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+            ))) ??
+        false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () {
+        return _onwill();
+      },
+      child: Scaffold(
+        backgroundColor: AppColor.backgroundColor,
+        body: SafeArea(
+          child: YoutubePlayerBuilder(
+              player: YoutubePlayer(controller: _controller),
+              builder: (context, player) {
+                return Scaffold(
+                  body: Container(
+                    child: player,
+                  ),
+                );
+              }),
+        ),
       ),
     );
   }
